974ebb66b7cafc9f4538baa3c5962b0a65128563
Felps7k <felipefarias97@icloud.com>
2022-07-29 00:19:00 -0300

    :construction: Extraindo dados da API e mostrando em tela.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ const fetchPokemon = async (pokemon) =>   {
     const data = await APIResponse.json();
     return data;
 }
+
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+}
+
+renderPokemon('gengar')

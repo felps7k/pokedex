667b64066b3fd4930338c74ea10da3244cab6963
Felps7k <felipefarias97@icloud.com>
2022-07-29 00:16:44 -0300

    :construction: Configurando chamada da API PokeAPI no JavaScript.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const fetchPokemon = async (pokemon) =>   {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
